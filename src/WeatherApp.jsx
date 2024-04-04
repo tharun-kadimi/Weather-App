72192d7f9285b628cf0ffccd7e40a893c751cfe3
tharun-kadimi <tharunkadimi13@gmail.com>
2024-04-04 23:19:07 +0530

    weather app updated

--- src/WeatherApp.jsx
+++ src/WeatherApp.jsx
@@ -2,7 +2,7 @@
 import React, { useState } from "react";
 import "./WeatherApp.css";
 
-const API_KEY = "37c5a2ed4b4104669c6c20fdb2cb9a85";
+const API_KEY = "65acab07d7b1b8d8f64c215ff73242fd";
 
 const WeatherApp = () => {
   const [city, setCity] = useState("");
@@ -26,26 +26,28 @@ const WeatherApp = () => {
   };
 
   return (
-    <div className="weather-app">
-      <h2>Weather App</h2>
-      <input
-        type="text"
-        value={city}
-        placeholder="Enter city name"
-        onChange={(e) => setCity(e.target.value)}
-      />
-      <button onClick={fetchWeather}>Get Weather</button>
-      {weather && (
-        <div className="weather-info">
-          <h3>
-            {weather.name}, {weather.sys.country}
-          </h3>
-          <p>Temperature: {weather.main.temp}°C</p>
-          <p>Description: {weather.weather[0].description}</p>
-          <p>Humidity: {weather.main.humidity}%</p>
-          <p>Wind Speed: {weather.wind.speed} m/s</p>
-        </div>
-      )}
+    <div className="bg-container">
+      <div className="weather-app">
+        <h2>Weather App</h2>
+        <input
+          type="text"
+          value={city}
+          placeholder="Enter Any city in the world"
+          onChange={(e) => setCity(e.target.value)}
+        />
+        <button onClick={fetchWeather}>Get Weather</button>
+        {weather && (
+          <div className="weather-info">
+            <h3>
+              {weather.name}, {weather.sys.country}
+            </h3>
+            <p>Temperature: {weather.main.temp}°C</p>
+            <p>Description: {weather.weather[0].description}</p>
+            <p>Humidity: {weather.main.humidity}%</p>
+            <p>Wind Speed: {weather.wind.speed} m/s</p>
+          </div>
+        )}
+      </div>
     </div>
   );
 };
